f5580744ad958abe985f36ce77869fdfc18574d2
matheus.silva <matheus.silva@icolabora.com>
2022-04-25 13:45:55 -0300

    Atualizar token reativo

--- src/stores/token.ts
+++ src/stores/token.ts
@@ -3,13 +3,14 @@ import { defineStore } from 'pinia'
 export const useTokenStore = defineStore({
   id: 'token',
   state: () => ({
-    token: localStorage.getItem("acessToken") || ""
+    token: localStorage.getItem("acessToken")
   }),
   getters: {
     getToken: (state) => state.token
   },
   actions: {
     setToken(val: string) {
+      this.token = val
       localStorage.setItem("acessToken", val)
     }
   }
